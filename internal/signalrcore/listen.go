6591f7906dc4fc1976dfc49a9d4a3dd6b5474a73
Julian Romeis <julianmaxromeis@gmail.com>
2026-04-21 03:38:20 +0200

    feat: implement background read loop for message processing
    - implemented 0x1e record separator for splitting batched messages
    
    - future replacement for the concurrent read loop in .On

---
+++ internal/signalrcore/listen.go
@@ -0,0 +1,62 @@
+package signalrcore
+
+import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"time"
+)
+
+func (c *Client) listen() {
+	defer func() {
+		close(c.doneChan)
+		c.cleanUp()
+	}()
+
+	for {
+		if err := c.conn.SetReadDeadline(time.Now().Add(c.idleTimeout)); err != nil {
+			c.errorMu.Lock()
+			c.err = errors.New("cannot set read deadline")
+			c.errorMu.Unlock()
+			return
+		}
+
+		_, data, err := c.conn.ReadMessage()
+		if err != nil {
+			c.errorMu.Lock()
+			c.err = err
+			c.errorMu.Unlock()
+			return
+		}
+
+		for sub := range bytes.SplitSeq(data, []byte{0x1e}) {
+			if len(sub) <= 0 {
+				continue
+			}
+
+			var msg Message
+			if err := json.Unmarshal(sub, &msg); err != nil {
+				c.logger.Error("failed to parse message", "raw_message", msg)
+				continue
+			}
+
+			switch msg.Type {
+			case 1:
+				c.handleInvocation(msg)
+			case 3:
+				c.handleCompletion(msg)
+			case 6:
+				c.logger.Debug("ping received")
+			case 7:
+				c.handleClose(msg)
+				return
+			default:
+				c.logger.Error("message type unsupported",
+					"type", msg.Type,
+					"target", msg.Target,
+					"raw_message", msg,
+				)
+			}
+		}
+	}
+}
